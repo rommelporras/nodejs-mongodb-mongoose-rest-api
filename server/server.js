98bf7c8c634291a6fabe2f7aca7a9368e870b539
Rommel Porras <rporras@yondu.com>
2018-10-16 19:29:16 +0800

    Text POST /todos routes

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
+module.exports = {app};
+
 /** 070 - Setting Up Mongoose **/
 // const newTodo = new Todo({
 //   text: 'Cook dinner'
