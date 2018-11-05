e1d3ac703711783b8d6c6ee0f28748f65833fd6f
Rommel Porras <rporras@yondu.com>
2018-11-05 18:28:41 +0800

    Add User model validation. Add email unique validator and password validator

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minLength: 1
-  }
+    minLength: 1,
+    unique: true,
+    validate: {
+      // validator: (value) => {
+      //   return validator.isEmail(value);
+      // },
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = User;
